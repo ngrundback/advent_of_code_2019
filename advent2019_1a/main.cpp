2b9559e0ebe034c5b243aca653dda0ccc5d5a18a
Nils Grundbäck <ngrundback@gmail.com>
2019-12-03 01:37:32 +0100

    correct answer for day 1a. Fix compile flags.

--- advent2019_1a/main.cpp
+++ advent2019_1a/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
+#include <vector>
+#include <string>
 
-
-static int calculateFuel(int mass)
+static int calculateFuelPerModule(int mass)
 {
     /* Fuel required to launch a given module is based on its mass.
      * Specifically, to find the fuel required for a module, 
@@ -20,10 +22,21 @@ static int calculateFuel(int mass)
     return (int)fuel;
 }
 
-static bool runTest(int expected, int input)
+int calculateTotalFuel(std::vector<std::string> vectorMass)
+{
+    int totalFuel = 0;
+    for (auto iterMass = vectorMass.begin(); iterMass != vectorMass.end(); ++iterMass)
+    {
+        totalFuel += calculateFuelPerModule(std::stoi(*iterMass));
+    }
+
+    return totalFuel;
+}
+
+static bool runTestPerModule(int expected, int input)
 {
     static int count = 1;
-    int answer = calculateFuel(input);
+    int answer = calculateFuelPerModule(input);
     if (expected == answer)
     {
         std::cout << "test:"<< count << " PASSED" << ", input: " << input 
@@ -38,25 +51,62 @@ static bool runTest(int expected, int input)
     }
 }
 
+static bool runTestOnVector(int expected, std::vector<std::string> testData)
+{
+    int answer = calculateTotalFuel(testData);
+    if (expected == answer)
+    {
+        std::cout << "vector test:" << " PASSED" << ", got: " << answer << "\n";
+        return true;
+    }
+    else
+    {
+        std::cout << "vector test:" << " FAILED" << ", got: " << answer << ", expected: " << expected << "\n";
+        return false;
+    }
+}
 
 static void runUnitTests()
 {
-    bool result = true;
+    bool result = true; 
 
-    result &= runTest(2, 12);
-    result &= runTest(2, 14);
-    result &= runTest(654, 1969);
-    result &= runTest(33583, 100756);
+    // Individual module calculations
+    result &= runTestPerModule(2, 12);
+    result &= runTestPerModule(2, 14);
+    result &= runTestPerModule(654, 1969);
+    result &= runTestPerModule(33583, 100756);
+
+    // Simulate an input.txt file with text strings
+    std::vector<std::string> testData;
+    testData.push_back("12");
+    testData.push_back("14");
+    testData.push_back("1969");
+    testData.push_back("100756");
+
+    result &= runTestOnVector(34241, testData);
 
     std::cout << "Unit tests result: " << (result ? "PASSED" : "FAILED") << "\n";
 }
 
 int main (int argc, char *argv[])
 {
-    // print bool values as 'true/false' instead of '1/0'
-    std::cout.setf(std::ios::boolalpha);
- 
     runUnitTests();
+    //exit(0); // Only run tests
+    
+    // Read in input.txt to a vector of strings
+    std::ifstream infile("input.txt");
+    std::string line;
+    std::vector<std::string> realData;
+   
+    while (infile >> line)
+    {
+        std::cout << "processing \"" << line << "\"\n";
+        realData.push_back(line);
+    }
+
+    int neededFuel = calculateTotalFuel(realData);
+
+    std::cout << "Total fuel needed: " << neededFuel << "\n";
 
     return 0;
 }
